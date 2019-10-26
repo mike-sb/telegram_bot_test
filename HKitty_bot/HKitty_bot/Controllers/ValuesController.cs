5be4fd99fe5e57ec7d14837f8483707741a02de3
RetroSpectro <on_desk@mail.ru>
2019-10-26 22:46:06 +0300

    add

---
+++ HKitty_bot/HKitty_bot/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HKitty_bot.Models;
+using Telegram.Bot.Types;
+
+namespace HKitty_bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        // GET api/values
+
+
+        // POST api/values
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody]Update update)
+        {
+            if (update == null) return Ok();
+
+            var commands = Bot.Commands;
+            var message = update.Message;
+            var botClient = await Bot.GetBotClientAsync();
+
+            foreach (var command in commands)
+            {
+                if (command.Contains(message))
+                {
+                    await command.Execute(message, botClient);
+                    break;
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ HKitty_bot/HKitty_bot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HKitty_bot.Models
+{
+    public class AppSettings
+    {
+        public static string Url { get; set; } = "https://URL:443/{0}";
+        public static string Name { get; set; } = "HKitty_bot";
+        public static string Key { get; set; } = "1003062132:AAFA2qbhYRE7J5l2TPWfVay7j96yBx3VD8Q";
+    }
+}
